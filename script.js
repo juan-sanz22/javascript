46e161129725bb6cd5ace351fb519e024e84dc83
juan_sanz <juan_sanz@estudante.sesisenai.org.br>
2025-03-19 10:13:49 -0300

    UPDATE- Fiz a atividade 2

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function verificarPar (numero){
-    if(numero % 2 === 0) {
-        console.log('par');
-    }else{
-        console.log(ímpar)
-    }
+function calcular (n1,n2){
+    const soma = n1 + n2;
+    const divisao = n1 / n2;
+    const multiplicacao = n1 * n2;
+    const subtracao = n1 - n2;  
+
+    console.log (soma);
+    console.log (subtracao);
+    console.log (multiplicacao);
+    console.log (divisao);
 }
 
-verificarPar(6);
+let n1 = parseFloat(prompt("digite o seu primeiro número"));
+let n2 = parseFloat(prompt("digite o seu segundo número"));
+calcular (n1,n2);
